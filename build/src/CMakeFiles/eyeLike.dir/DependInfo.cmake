a069818bfc9a4bad290faae87a84115216adca37
Vladimir Bazareuski <vladimirbazareuski@192.168.1.34>
2016-11-08 19:16:41 +0300

    Initial commit.

---
+++ build/src/CMakeFiles/eyeLike.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/vladimirbazareuski/dev/eyeLike/src/findEyeCenter.cpp" "/Users/vladimirbazareuski/dev/eyeLike/build/src/CMakeFiles/eyeLike.dir/findEyeCenter.cpp.o"
+  "/Users/vladimirbazareuski/dev/eyeLike/src/findEyeCorner.cpp" "/Users/vladimirbazareuski/dev/eyeLike/build/src/CMakeFiles/eyeLike.dir/findEyeCorner.cpp.o"
+  "/Users/vladimirbazareuski/dev/eyeLike/src/helpers.cpp" "/Users/vladimirbazareuski/dev/eyeLike/build/src/CMakeFiles/eyeLike.dir/helpers.cpp.o"
+  "/Users/vladimirbazareuski/dev/eyeLike/src/main.cpp" "/Users/vladimirbazareuski/dev/eyeLike/build/src/CMakeFiles/eyeLike.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv3/3.1.0_4/include/opencv"
+  "/usr/local/Cellar/opencv3/3.1.0_4/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
